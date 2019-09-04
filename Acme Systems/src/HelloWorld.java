1d1881a7b6e1e8352d1ba16373e9007433c9d1af
tristanwalk <tristanwalk1@gmail.com>
2019-09-04 16:16:26 -0400

    Added heyNow method

--- Acme Systems/src/HelloWorld.java
+++ Acme Systems/src/HelloWorld.java
@@ -5,5 +5,10 @@ public class HelloWorld {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Hello from Github");
 	}
+	
+	public static void heyNow() {
+		String Msg = "Hey now!";
+		System.out.println(Msg);
+	}
 
 }
